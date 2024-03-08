cb4d7599a7a69def8f5d4ecacf6af59247526422
hayeon <kimma0514@gmail.com>
2024-03-08 14:31:21 +0900

    프로젝트 생성

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<div>test</div>
+		</div>
+	);
+}
+
+export default App;
